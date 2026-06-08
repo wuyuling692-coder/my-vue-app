de7efc2204894185b104a52f0631a54c7222d9ac
zhanglan <cdzhanglan@gmail.com>
2026-06-08 21:14:44 +0800

    修复setup语法错误和defineExpose导入问题

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,45 @@
 <template>
-    <div  class="app">
-        
-        <Person></Person>
+    <div>
+        <h2 ref="title2">牙齿p p</h2>  
+        <button @click="showLog">点我输出h2这个元素</button>  
+       <Person ref="ren"></Person>
     </div>
 </template>
 
-<script lang="ts">
-    import { defineComponent } from 'vue'
+<script lang="ts" setup name="App">
+// ref属性：在Vue3里，导入ref方法，直接用defineExpose
+// 可以直接导出所有的值，主Vue里导入分支vue里的数据，通
+// 过ref获取组件实例对象。
     import Person from './components/Person.vue'
+    import {ref}from'vue'
+    let title2=ref()
+    let ren=ref()
+    function showLog(){
+        console.log('组件实例:', ren.value)
+        console.log('a =',ren.value.showLog())
+        console.log('b =')
+        console.log('c =')
+    }
 
-    export default defineComponent({
-        name:'App',
-        components:{Person}
-    })
 </script>
+
+<style scoped>
+.person {
+    background-color: skyblue;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+}
+
+.game-list {
+    margin-top: 20px;
+}
+
+button {
+    margin: 5px;
+    padding: 6px 12px;
+    cursor: pointer;
+}
+</style>
+
 
--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,12 +1,25 @@
 <template>
     <div class="person">
-                 
+        <h1>中国</h1>
+        <h2 ref="title2">北京</h2>
+        <button @click="showLog">点我输出h2这个元素</button>                
     </div>   
 </template>
+// ref属性：在Vue3里，导入ref方法，直接用defineExpose
+// 可以直接导出所有的值，主Vue里导入分支vue里的数据，通
+// 过ref获取组件实例对象。
 
-<script lang="ts" setup>
-import { reactive,watch,computed } from 'vue'
+<script lang="ts" setup name="Person">
+    import { ref } from 'vue'
+    let title2 = ref()
+    let a = ref(0)
+    let b = ref(1)
+    let c = ref(2)
 
+    function showLog(){
+        console.log(title2.value)
+    }
+    defineExpose({a,b,c,showLog})
 
 
 </script>
