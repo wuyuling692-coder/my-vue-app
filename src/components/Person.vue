cf806763f1c0bf5a6fc943237aeeab686236d483
zhanglan <cdzhanglan@gmail.com>
2026-06-08 21:55:13 +0800

    修复类型定义：Persons改为数组类型，import格式规范化

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,26 +1,17 @@
 <template>
     <div class="person">
-        <h1>中国</h1>
-        <h2 ref="title2">北京</h2>
-        <button @click="showLog">点我输出h2这个元素</button>                
+                   
     </div>   
 </template>
-// ref属性：在Vue3里，导入ref方法，直接用defineExpose
-// 可以直接导出所有的值，主Vue里导入分支vue里的数据，通
-// 过ref获取组件实例对象。
 
 <script lang="ts" setup name="Person">
-    import { ref } from 'vue'
-    let title2 = ref()
-    let a = ref(0)
-    let b = ref(1)
-    let c = ref(2)
+    import { type PersonInter, type Persons } from '@/types';
 
-    function showLog(){
-        console.log(title2.value)
-    }
-    defineExpose({a,b,c,showLog})
-
+    let PersonList :Persons = [
+        {id:'asyud7asfd01',name:'王一',age:18},
+        {id:'asyud7asfd02',name:'王三',age:17},
+        {id:'asyud7asfd03',name:'王五',age:88}
+    ]
 
 </script>
 
